9f54d0aeb629acc4d474b3030b5a686388ee3c0c
Shahid-sk <mail2sk.shahid@gmail.com>
2021-09-18 20:00:35 +0530

    added: created a class for file operations

--- src/com/bridgelabz/EmployeePayrollImpl.java
+++ src/com/bridgelabz/EmployeePayrollImpl.java
@@ -14,7 +14,7 @@ public class EmployeePayrollImpl {
 
 	/*
 	 * Purpose: Method to take employee details from console. and add those details
-	 * to list.
+	 * to List.
 	 */
 	public void readData() {
 		Scanner sc = new Scanner(System.in);
